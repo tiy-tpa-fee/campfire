66e454ae90f048c7f0725c6a49877ef24ecc8c46
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-18 15:42:54 -0500

    can input, need button ajax request

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+// onClick submit sends ajax request to JSON
 export default class Submit extends Component {
 
   render () {
-    return <main>
+    return <main className='submitMain'>
       <h2>Submit</h2>
+      <form className='submitForm'>
+        <input type='text' className='submitLink' defaultValue='http://...' />
+        <h5>Tag</h5>
+        <select className='tagSelector'>
+          <optgroup>
+            <option>Front End</option>
+            <option>Back End</option>
+          </optgroup>
+        </select>
+        <input type='text' className='otherTags' defaultValue='Tags' />
+        <textarea className='submitComment' rows='10' cols='30' defaultValue='Comments' />
+        <button type='submit' value='Submit'>Submit</button>
+      </form>
     </main>
   }
 }
