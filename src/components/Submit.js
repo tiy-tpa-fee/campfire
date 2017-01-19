48b203288a554a0f74ec6dfe3714f0476146557e
Liz Tiller <elizabeth.tiller@gmail.com>
2017-01-19 18:13:32 -0500

    just need API and styling

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -2,21 +2,53 @@ import React, { Component } from 'react'
 
 // onClick submit sends ajax request to JSON
 export default class Submit extends Component {
+  constructor () {
+    super()
+    // this.state = {
+    //   value: '',
+    //   tag: ''
+    // }
+
+    // this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  // handleChange (e) {
+  //   this.setState({value: e.target.value})
+  // }
+
+  handleSubmit (e) {
+    const data = {
+      url: this.refs.url.value,
+      primary_tag: this.refs.primaryTag.value,
+      tag_list: this.refs.tagList.value,
+      description: this.refs.description.value
+    }
+    console.log(data)
+    e.preventDefault()
+  }
 
   render () {
     return <main className='submitMain'>
       <h2>Submit</h2>
-      <form className='submitForm'>
-        <input type='text' className='submitLink' defaultValue='http://...' />
-        <h5>Tag</h5>
-        <select className='tagSelector'>
-          <optgroup>
-            <option>Front End</option>
-            <option>Back End</option>
-          </optgroup>
-        </select>
-        <input type='text' className='otherTags' defaultValue='Tags' />
-        <textarea className='submitComment' rows='10' cols='30' defaultValue='Comments' />
+      <form className='submitForm' onSubmit={this.handleSubmit}>
+        <label>
+          Link:
+          <input type='text' className='submitLink' placeholder='http://...' ref='url' />
+        </label>
+
+        <label>
+          Tag:
+          <select className='tagSelector' ref='primaryTag'>
+            <option value='FrontEnd'>Front End</option>
+            <option value='BackEnd'>Back End</option>
+          </select>
+        </label>
+
+        <input type='text' className='otherTags' placeholder='Other tags' ref='tagList' />
+
+        <textarea className='submitComment' rows='10' cols='30' placeholder='Comments' ref='description' />
+
         <button type='submit' value='Submit'>Submit</button>
       </form>
     </main>
