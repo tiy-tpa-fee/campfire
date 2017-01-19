52ebc32d1e724bec2fedceb5050e7d80c13a9efc
m1g <miguelmalcolm@outlook.com>
2017-01-18 20:47:19 -0500

    updated with basic mockup styles

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -6,20 +6,22 @@ export default class Trending extends Component {
     return (
 
       <div className='trending'>
-        <h2>Trending Posts</h2>
+        <h2>TRENDING</h2>
         <div className='trending-main'>
-          <img src='http://i.imgur.com/UGztbbl.png' alt='image of a car' />
+          <img src='http://i.imgur.com/YxNBH96.png' alt='image of a car' />
           <div>
+            <time>Date</time>
             <h2>Post Title</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima soluta officiis et, doloribus, modi dolores nulla nesciunt doloremque maiores quisquam recusandae esse quam repudiandae quas ipsum eius ratione architecto culpa!</p>
             <p>[upvotes, username, timestamp, login to comment]</p>
           </div>
         </div>
         <div className='trending-sub'>
-          <img src='http://i.imgur.com/bpGDcRA.png' alt='image of Js' />
+          <img src='http://i.imgur.com/peSY1P2.png' alt='image of Js' />
           <div>
-            <h2>Post Title</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima soluta officiis et, doloribus, modi dolores nulla nesciunt doloremque maiores quisquam recusandae esse quam repudiandae quas ipsum eius ratione architecto culpa!</p>
+            <time>Date</time>
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima soluta officiis et, doloribus</p>
             <p>[upvotes, username, timestamp, login to comment]</p>
           </div>
         </div>
