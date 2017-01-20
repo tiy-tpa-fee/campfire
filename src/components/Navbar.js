1ac029653cb4069e3522a1d2871bc7a4f86b33c9
Jason L Perry <jasper@ambethia.com>
2017-01-20 11:11:16 -0500

    Hello, API

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import API from '../utils/API'
 
 export default class Navbar extends Component {
 
@@ -8,8 +10,13 @@ export default class Navbar extends Component {
   constructor () {
     super()
     this.state = {
-      visible: false
+      visible: false,
+      categories: []
     }
+  }
+
+  componentDidMount () {
+    API.get('/tags?primary=true').then(tags => this.setState({ categories: tags }))
   }
 
   toggle () {
@@ -17,15 +24,21 @@ export default class Navbar extends Component {
   }
 
   render () {
+    const categories = this.state.categories.map((category, i) => {
+      return <li>
+        <Link className='category-nav' to={'/c/' + category.name}>
+          {category.name.toUpperCase()}
+        </Link>
+      </li>
+    })
+
     return <div className='navBar'>
       <nav className={this.state.visible ? '' : 'hidden'}>
         <ul>
           <li><button onClick={() => this.toggle()}>x</button></li>
           <li><input type='text' name='search' placeholder='Search..' /><img src='http://uploads.webflow.com/55cf187a8cacc9ab28d524b9/55d243680a3ebde1455ffab4_transparency.png' width='15' /></li>
-          <li><button className='home-nav'onClick={(e) => e.preventDefault()}>Home</button></li>
-          <li><button className='category-nav' onClick={(e) => e.preventDefault()}>Category</button></li>
-          <li><button className='frontend-nav' onClick={(e) => e.preventDefault()}>Front-End</button></li>
-          <li><button className='backend-nav' onClick={(e) => e.preventDefault()}>Back-End</button></li>
+          <li><Link className='home-nav' to='/'>Home</Link></li>
+          {categories}
         </ul>
       </nav>
       <button className='menu' onClick={() => this.toggle()}>MENU</button>
